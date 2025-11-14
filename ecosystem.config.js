75858da6710063caba043f0582b33cb03d01f75c
nazif67 <naziftekinalp67@gmail.com>
2025-11-14 16:00:50 +0300

    Update: ahikurumsal.com konfigurasyonu - Port 3003

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     apps: [
       {
-        name: "ahikariyer-front",
-        cwd: "/home/ahikariyer-front",
+        name: "ahikurumsal-frontend",
+        cwd: "/var/www/ahikurumsal/frontend",
         script: "node_modules/next/dist/bin/next",
         args: "start",
         instances: 1,
@@ -11,7 +11,7 @@ module.exports = {
         max_memory_restart: "512M",
         env: {
           NODE_ENV: "production",
-          PORT: 3001
+          PORT: 3003
         }
       }
     ]
